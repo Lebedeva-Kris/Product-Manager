39781ffbebe2f0d63b6e2a8f93c4f2ade96b24fe
Kristina <kris2015.lebedeva@yandex.ru>
2020-07-23 23:58:32 +0300

    search.equalsIgnoreCase("") deleted

--- src/main/java/ru/netology/domain/Product.java
+++ src/main/java/ru/netology/domain/Product.java
@@ -15,8 +15,9 @@ public class Product {
     }
 
     public boolean matches(String search) {
-        if (search.equalsIgnoreCase(name))
+        if (search.equalsIgnoreCase(name)) {
             return true;
+        }
         return false;
     }
 
